0090cd64007870c3377d0f5e01dd2e3e05953379
Surya Vedantam <surya.phani1729@gmail.com>
2020-05-01 14:59:19 +0530

    first

---
+++ build/precache-manifest.c4e66f7fbd2eabe3077224c65b0bca9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dff39278a68a04b65efde3baa6144c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28d8fd2401d1a91ab1ba",
+    "url": "/static/css/main.170f56e8.chunk.css"
+  },
+  {
+    "revision": "1a12cf66a026013ec46e",
+    "url": "/static/js/2.1a12f996.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1a12f996.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28d8fd2401d1a91ab1ba",
+    "url": "/static/js/main.d9495035.chunk.js"
+  },
+  {
+    "revision": "008cd249cffa12f2634e",
+    "url": "/static/js/runtime-main.f5387fb8.js"
+  }
+]);
